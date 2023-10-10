2d2cb95ab8adb3f13cdc7b1649873258fc1e91bb
Matheus Oliveira <matheus.oliveira199768@gmail.com>
2023-10-10 07:00:53 -0300

    add model users

---
+++ src/main/java/com/mt01/todolist/user/UserModel.java
@@ -0,0 +1,17 @@
+package com.mt01.todolist.user;
+
+public class UserModel {
+  private String username;
+  private String name;
+  private String password;
+
+  public String getUsername() {
+    return username;
+  }
+  public String getName() {
+    return name;
+  }
+  public String getPassword() {
+    return password;
+  }  
+}

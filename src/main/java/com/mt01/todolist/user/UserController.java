1c60ded5be0d69b5c00ef2bafe032f9cd1477e76
Matheus Oliveira <matheus.oliveira199768@gmail.com>
2023-10-10 06:59:31 -0300

    add controller

---
+++ src/main/java/com/mt01/todolist/user/UserController.java
@@ -0,0 +1,13 @@
+package com.mt01.todolist.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+  
+  public void create(){
+    
+  }
+}

c7407e55a7a459b560c42ac92667decc031baef2
Matheus Oliveira <matheus.oliveira199768@gmail.com>
2023-10-11 20:40:21 -0300

    add lombok

--- src/main/java/com/mt01/todolist/user/UserModel.java
+++ src/main/java/com/mt01/todolist/user/UserModel.java
@@ -1,19 +1,10 @@
 package com.mt01.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
   private String username;
   private String name;
   private String password;
-
-  public String getUsername() {
-    return username;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String getPassword() {
-    return password;
-  }
 }

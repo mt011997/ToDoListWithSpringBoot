7ea3488d2b2036c3b7c91e6b0fceb70073680fcb
Matheus Oliveira <matheus.oliveira199768@gmail.com>
2023-10-11 21:24:07 -0300

    add username unique

--- src/main/java/com/mt01/todolist/user/UserController.java
+++ src/main/java/com/mt01/todolist/user/UserController.java
@@ -1,6 +1,11 @@
 package com.mt01.todolist.user;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +19,17 @@ public class UserController {
   private IUserRepository userRepository;
 
   @PostMapping("/")
-  public UserModel create(@RequestBody UserModel userModel) {
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if (user != null) {
+      Map<String, String> responseBody = new HashMap<>();
+      responseBody.put("message", "Usuário já existe!");
+      return ResponseEntity.status(HttpStatus.CONFLICT).body(responseBody);
+    }
+
     var userCreated = this.userRepository.save(userModel);
-    return userCreated;
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 }

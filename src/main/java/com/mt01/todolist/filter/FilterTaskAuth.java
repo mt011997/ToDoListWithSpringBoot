6f7587c388580facbce7e0b4f6ddb9795569f0bb
Matheus Oliveira <matheus.oliveira199768@gmail.com>
2023-10-13 05:56:36 -0300

    passwrd decode

--- src/main/java/com/mt01/todolist/filter/FilterTaskAuth.java
+++ src/main/java/com/mt01/todolist/filter/FilterTaskAuth.java
@@ -3,9 +3,13 @@ package com.mt01.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.mt01.todolist.user.IUserRepository;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 
@@ -15,22 +19,34 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
 
+  @Autowired
+  private IUserRepository userRepository;
+
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-    
+
     var authorization = request.getHeader("Authorization");
     var authEncoded = authorization.substring("Basic".length()).trim();
-    
+
     byte[] authDecded = Base64.getDecoder().decode(authEncoded);
 
     var authString = new String(authDecded);
     String[] credentials = authString.split(":");
-    
+
     String username = credentials[0];
-    String passwrd = credentials[1];
+    String password = credentials[1];
 
-    filterChain.doFilter(request, response);
+    var user = this.userRepository.findByUsername(username);
+    if (user == null) {
+      response.sendError(401);
+    } else {
+      var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+      if (passwordVerify.verified) {
+        filterChain.doFilter(request, response);
+      } else {
+        response.sendError(401);
+      }
+    }
   }
-
 }
